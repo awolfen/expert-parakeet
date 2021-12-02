42188ab267d353ae39b89b9f39c7cd43c51b5f52
awolfen <alfie.wolfenden@gmail.com>
2021-12-02 21:08:04 +0000

    with toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const loggedIn = useSelector(state => state.auth.loggedIn);
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!loggedIn && <Auth />}
+      {loggedIn && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,16 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+import counterReducer from './counter';
+import authReducer from './auth';
 
-const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "increment":
-            return {
-                //state.counter++ should NOT be used, we do not want to mutate the existing state with from a reducer function
-                counter: state.counter + 1,
-                showCounter: state.showCounter
-            };
-        case "increase":
-            return {
-                counter: state.counter + action.amount,
-                showCounter: state.showCounter
-            };
-        case "decrement":
-            return {
-                counter: state.counter - 1,
-                showCounter: state.showCounter
-            };
-        case "toggle":
-            return {
-                counter: state.counter,
-                showCounter: !state.showCounter
-            }
-        default:
-            return state;
-    };
-}
 
-const store = createStore(counterReducer);
+
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer
+    }
+});
 
 export default store;
